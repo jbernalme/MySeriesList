1db5868dae7be07a37faffb3856df37517474271
Jesus David Bernal <jbernalme@gmail.com>
2026-01-13 11:38:20 -0500

    update dockerfile

--- app/Http/Controllers/MyListController.php
+++ app/Http/Controllers/MyListController.php
@@ -13,24 +13,52 @@ use Illuminate\Support\Facades\Log;
 
 class MyListController extends Controller
 {
+    // public function show($username)
+    // {
+    //     try {
+
+    //         $user_id = User::where('username', $username)->firstOrFail()->id;
+            
+
+    //         $stateWatchingList = WatchingState::all(['id', 'name', 'color']);
+
+    //         $tv = TvList::where('user_id', $user_id)
+    //             ->select(['id', 'name', 'api_id', 'poster', 'watching_state_id', 'score_id', 'season', 'episode'])
+    //             ->selectRaw('"TvShow" as type');
+
+    //         $lists = MovieList::where('user_id', $user_id)
+    //             ->select(['id', 'name', 'api_id', 'poster', 'watching_state_id', 'score_id'])
+    //             ->selectRaw('Null as season, NULL as episode, "Movie" as type')
+    //             ->union($tv)
+    //             // ->orderBy($this->sortField, $this->sortDirection)
+    //             ->get();
+
+    //     } catch (ModelNotFoundException $e) {
+    //         return Inertia::render('Error', ['status' => 404]);
+    //     }
+
+    //     $data = collect(['lists' => $lists])->merge(['stateWatchingList' => $stateWatchingList]);
+
+    //     // Log::debug($data);
+    //     return Inertia::render('MyList/Index', ['data' => $data]);
+    // }
+
     public function show($username)
     {
         try {
-
-            $user_id = User::where('username', $username)->firstOrFail()->id;
-            
+            $user = User::where('username', $username)->firstOrFail();
+            $user_id = $user->id;
 
             $stateWatchingList = WatchingState::all(['id', 'name', 'color']);
 
             $tv = TvList::where('user_id', $user_id)
                 ->select(['id', 'name', 'api_id', 'poster', 'watching_state_id', 'score_id', 'season', 'episode'])
-                ->selectRaw('"TvShow" as type');
+                ->selectRaw("'TvShow' as type");
 
             $lists = MovieList::where('user_id', $user_id)
                 ->select(['id', 'name', 'api_id', 'poster', 'watching_state_id', 'score_id'])
-                ->selectRaw('Null as season, NULL as episode, "Movie" as type')
+                ->selectRaw("NULL as season, NULL as episode, 'Movie' as type")
                 ->union($tv)
-                // ->orderBy($this->sortField, $this->sortDirection)
                 ->get();
 
         } catch (ModelNotFoundException $e) {
@@ -39,34 +67,6 @@ class MyListController extends Controller
 
         $data = collect(['lists' => $lists])->merge(['stateWatchingList' => $stateWatchingList]);
 
-        // Log::debug($data);
         return Inertia::render('MyList/Index', ['data' => $data]);
     }
-
-    public function showtest($username)
-{
-    try {
-        $user = User::where('username', $username)->firstOrFail();
-        $user_id = $user->id;
-
-        $stateWatchingList = WatchingState::all(['id', 'name', 'color']);
-
-        $tv = TvList::where('user_id', $user_id)
-            ->select(['id', 'name', 'api_id', 'poster', 'watching_state_id', 'score_id', 'season', 'episode'])
-            ->selectRaw("'TvShow' as type");
-
-        $lists = MovieList::where('user_id', $user_id)
-            ->select(['id', 'name', 'api_id', 'poster', 'watching_state_id', 'score_id'])
-            ->selectRaw("NULL as season, NULL as episode, 'Movie' as type")
-            ->union($tv)
-            ->get();
-
-    } catch (ModelNotFoundException $e) {
-        return Inertia::render('Error', ['status' => 404]);
-    }
-
-    $data = collect(['lists' => $lists])->merge(['stateWatchingList' => $stateWatchingList]);
-
-    return Inertia::render('MyList/Index', ['data' => $data]);
-}
 }
