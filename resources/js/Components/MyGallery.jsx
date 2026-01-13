9ca34bdc2559089287e4484d1eec5772ed8dd756
Jesus David Bernal <jbernalme@gmail.com>
2026-01-13 13:29:37 -0500

    test gallery

--- resources/js/Components/MyGallery.jsx
+++ resources/js/Components/MyGallery.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import FsLightbox from "fslightbox-react";
+// import FsLightbox from "fslightbox-react";
 
 const MyGallery = ({ data, type = "image" }) => {
     // data example
@@ -43,16 +43,11 @@ const MyGallery = ({ data, type = "image" }) => {
                             </figure>
                         ))}
                     </div>
-                    <FsLightbox
+                    {/* <FsLightbox
                         sourceIndex={currentIndex}
                         toggler={toggler}
-                        // sources={getSource(type, data)}
-                        sources={[
-                            "https://i.imgur.com/fsyrScY.jpg",
-                            "https://www.youtube.com/watch?v=3nQNiWdeH2Q",
-                            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-                        ]}
-                    />
+                        sources={getSource(type, data)}
+                    /> */}
                 </>
             ) : (
                 <h2 className="mt-20 text-2xl text-center ">
